c214c486d914fc3c62543d570004dc168f493d17
Nick Eisner <nickwe02@gmail.com>
2021-10-26 15:58:30 -0500

    Update Game.cs
    Reformatted menu to a dynamic interactive menu solution I had from a previous project.

--- CraftMagicalPuppets/Game.cs
+++ CraftMagicalPuppets/Game.cs
@@ -3,54 +3,70 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using static System.Console;
+using static CraftMagicalPuppets.Utility;
 
 namespace CraftMagicalPuppets
 {
     public class Game
     {
         public Player CurrentPlayer;
+        private Store store = new Store();
         public string GameTitle;
         public bool ContinuePlay = true;
+        
 
         public Game()
         {
             GameTitle = "Puppet Crafter";
-
+            Title = GameTitle;
+            Print = PrintCommandLine;
         }
         public void StartGame()
         {
             Write("Please enter your name: ");
             string nameResponse = ReadLine();
             CurrentPlayer = new Player(nameResponse);
-
-            WriteLine($"Welcome to {GameTitle}, {CurrentPlayer.DisplayName()}\n");
-
+            Print($"Welcome to {GameTitle}, {CurrentPlayer.DisplayName()}\n");
+            ReadKey();
             while (ContinuePlay)
             {
-                WriteLine("Would you like to.... (Enter Number That Corrisponds To The Option You Want)");
-                WriteLine("1) View Inventory");
-                WriteLine("2) View Recipies\n");
-                string playerResponse = ReadLine();
-                int playResponse = Convert.ToInt32(playerResponse);
-                switch (playResponse)
-                {
-                    case 1:
-                        CurrentPlayer.ViewInventory();
-                        break;
 
-                    case 2:
-                        CurrentPlayer.ViewRecipes();
+                string text = "Would you like to....";
+                List<string> mainMenuOptions = new List<string>{ "View Inventroy", "View Recipies", "Craft Puppet", "Shop", "Exitgame" };
+                Menu mainMenu = new Menu(text, mainMenuOptions);
+                int mainMenuSelectedIndex = mainMenu.Run(ConsoleColor.Red);
+                switch (mainMenuSelectedIndex)
+                {
+                    case 0:
+                        CurrentPlayer.ViewInventory();
+                        Clear();
                         break;
-                    default:
-                        WriteLine("Please enter a valid option.");
+                    case 1:
+                        CurrentPlayer.ViewRecipes();
+                        Clear();
+                        break;
+                    case 2:
+                        CurrentPlayer.CraftPuppet();
+                        Clear();
+                        break;
+                    case 3:
+                        store.Start(CurrentPlayer);
+                        Clear();
+                        break;
+                    case 4:
+                        ContinuePlay = false;
+                        Clear();
+                        Print("See you next time!");
                         ReadKey();
                         break;
-                        
+                    default:
+                        Print("Please enter a valid option.");
+                        ReadKey();
+                        Clear();
+                        break;
+
                 }
-
             }
-
-
         }
     }
 }
