9289f2b2f575ae4654bac1c544a4b4216221265b
Nick Eisner <nickwe02@gmail.com>
2021-10-29 17:46:29 -0500

    Added Menus Class
    Added a class to handle the code for the various menus in the program.

---
+++ CraftMagicalPuppets/Menus.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CraftMagicalPuppets
+{
+    public class Menus
+    {
+        public static int CraftPuppetMenu(List<Recipe> recipes)
+        {
+            string text = "What puppet would you like to craft?";
+            List<string> mainMenuOptions = new List<string>();
+            foreach (Recipe r in recipes)
+            {
+                mainMenuOptions.Add(r.Name);
+            }
+            Menu mainMenu = new Menu(text, mainMenuOptions);
+            int mainMenuSelectedIndex = mainMenu.Run(ConsoleColor.Red);
+            return mainMenuSelectedIndex;
+        }
+    }
+}
