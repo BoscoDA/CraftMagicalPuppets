f9719c9387074de20d4c5585a2f8b9881e4a9b9e
Nick Eisner <nickwe02@gmail.com>
2021-10-29 17:50:00 -0500

    Update Program.cs

--- CraftMagicalPuppets/Program.cs
+++ CraftMagicalPuppets/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Console;
 
 namespace CraftMagicalPuppets
 {
@@ -10,6 +11,21 @@ namespace CraftMagicalPuppets
     {
         static void Main(string[] args)
         {
+            CursorVisible = false;
+            int Height = 50;
+            int Width = 100;
+
+            try
+            {
+                SetWindowSize(Width, Height);
+            }
+            catch
+            {
+                WriteLine("Cannot create a big enough console window.");
+                WriteLine("Try adjusting your font/console settings and restarting.");
+                WriteLine("You can continue playing, just be aware that some art might not render properly!");
+                Utility.WaitForKey();
+            }
             Game game = new Game();
             game.StartGame();
         }
